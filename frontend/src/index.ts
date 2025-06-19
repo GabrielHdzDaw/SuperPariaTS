cca50e71c30e0f915d1b376ed4f9bc0788070273
GabrielHdzDaw <gabrielhernandezdaw@gmail.com>
2025-06-19 21:21:38 +0200

    added new shader effect and updated card flip animation

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -2,24 +2,35 @@ import Card from "./models/Card";
 import { getPairs } from "./utils/CreateDeck";
 
 
-// const playArea = document.getElementById("playArea");
-// const deck: Card[] = getPairs();
+const playArea = document.getElementById("playArea");
+const deck: Card[] = getPairs();
 
-// deck.forEach((card) => {
-//   const cardElement = document.createElement("div");
-//   cardElement.className = "card";
-//   cardElement.dataset.suit = card.suit;
-//   cardElement.dataset.value = card.value;
-//   cardElement.style.backgroundImage = `url(${card.currentImage})`;
-//   cardElement.title = `${card.suit}${card.value}`;
+deck.forEach((card) => {
+  const cardElement = document.createElement("div");
+  cardElement.className = "card";
 
-//   cardElement.addEventListener("click", () => {
-//     card.flip();
-//     cardElement.style.backgroundImage = `url(${card.currentImage})`;
-//   });
+  const cardInner = document.createElement("div");
+  cardInner.className = "card-inner";
 
-//   playArea?.appendChild(cardElement);
-// });
+  const cardFront = document.createElement("div");
+  cardFront.className = "card-front";
+  cardFront.style.backgroundImage = `url(${card.frontImage})`;
+
+  const cardBack = document.createElement("div");
+  cardBack.className = "card-back";
+  cardBack.style.backgroundImage = `url(${card.backImage})`;
+
+  cardInner.appendChild(cardFront);
+  cardInner.appendChild(cardBack);
+  cardElement.appendChild(cardInner);
+
+  cardElement.addEventListener("click", () => {
+    card.flip();
+    cardElement.classList.toggle("flipped");
+  });
+
+  playArea?.appendChild(cardElement);
+});
 
 import { startShaderBackground } from './backgroundShader';
 import fragShaderSrc from './assets/shader.frag?raw';
